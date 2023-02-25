5cf0d57de7223674fcc05ca6485d835d357b226f
Denver Casunuran <vercasunuran@gmail.com>
2023-02-25 18:40:40 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from './components/navbar';
+import { Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    
+    </div>
+  );
+}
+
+export default App;
